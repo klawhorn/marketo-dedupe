6e3618098d8d634a68f36684cc4d5d7e611b2795
Blake Johnson <blakejohnson.pdx@gmail.com>
2016-10-30 13:22:35 -0700

    has a dupe. need to see what I am passing in the email run

--- js/dedupe.js
+++ js/dedupe.js
@@ -8,19 +8,21 @@ module.exports = {
   //reads file passed to it, returns array of objects to filter
   deduplicate : function (file, callback) {
     fs.readFile(file, 'utf8', function(err, data) {
-      var removedItems = {};
-      removedItems.removedArray= [];
+      duplicate_array= [];
       //reads file
         var file_data = JSON.parse(data).leads;
       //reorders by id for filtering
-        var idOrder = reorderID.reOrder(file_data, '_id', removedItems.removedArray);
-      //reformats an array of unique id entries
-        var filtered_by_id = reset.resetFormat(idOrder);
+        var ordered_id_object = reorderID.reOrder(file_data, '_id');
+        console.log(ordered_id_object);
+        //reformats an array of unique id entries
+        var ordered_id_output = ordered_id_object.output_data;
+        var filtered_by_id = reset.resetFormat(ordered_id_output);
       //takes array of unique ids and reorders it by email
-        var emailOrder = reorderID.reOrder(file_data, 'email', removedItems.removedArray);
+        var ordered_email_object = reorderID.reOrder(file_data, 'email');
+        console.log(ordered_email_object);
+        var ordered_email_output = ordered_email_object.output_data;
       //reformats into an array of unique email entries
-        var filtered_by_email = reset.resetFormat(emailOrder);
-        console.log(removedItems.removedArray);
+        var filtered_by_email = reset.resetFormat(ordered_email_output);
 
         callback('deduplicatedData.json', filtered_by_email);
       });
--- js/filter.js
+++ js/filter.js
@@ -2,28 +2,36 @@ var choose = require('./chooseValue.js');
 var reset = require('./resetFormat.js');
 
 module.exports = {
-  reOrder: function (array, property, array) {
+  reOrder: function (array, property) {
     function reorder (arr) {
       //create an empty object for what we will keep
-      var obj = {};
+      var obj = {
+        'output_data' : {},
+        'duplicate_data' : {}
+      };
       //big array of data objects, for each source object
       arr.forEach(function (entry) {
         //if the new object doesn't already have a property with the id, put it in there
-        if (!obj[entry[property]]) {
-          obj[entry[property]] = [];
-          obj[entry[property]].push(entry);
+        if (!obj.output_data[entry[property]]) {
+          obj.output_data[entry[property]] = [];
+          obj.output_data[entry[property]].push(entry);
         } else {
           //if it's already there, look at the existing one's date and the current one's date.
-          var existing = obj[entry[property]][0].entryDate;
+          var existing = obj.output_data[entry[property]][0].entryDate;
           var current= entry.entryDate;
           //if the entry date are the same, current is lower on the list than existing, so pop out the existing entry and push the current one in.
           if (choose.isSame(existing, current) || choose.isBefore(existing, current)) {
             //THIS IS WHERE WE DEDUPLICATE, NEED TO STORE obj[entry[property]][0] SOMEWHERE.
-            array.push(obj[entry[property]].pop());
-            obj[entry[property]].push(entry)
+            if (obj.duplicate_data[entry[property]]) {
+              obj.duplicate_data[entry[property]].push(obj.output_data[entry[property]].pop());
+            } else {
+              obj.duplicate_data[entry[property]] = [];
+              obj.duplicate_data[entry[property]].push(obj.output_data[entry[property]].pop());
+            }
+            obj.output_data[entry[property]].push(entry);
           } else {
             //THIS IS WHERE WE DEDUPLICATE, NEED TO STORE CURRENT ENTRY SOMEWHERE (entry).
-            array.push(entry);
+            obj.duplicate_data[entry[property]].push(entry);
             return;
           }
         }

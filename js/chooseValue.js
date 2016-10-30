68cd19f68de102952ad35e1306c7584b49b3ecec
Blake Johnson <blakejohnson.pdx@gmail.com>
2016-10-29 18:51:27 -0700

    added chooseValue

---
+++ js/chooseValue.js
@@ -0,0 +1,5 @@
+module.exports = {
+  chooseByDate : function () {
+    console.log('date');
+  }
+}
--- js/readFile.js
+++ js/readFile.js
@@ -2,16 +2,16 @@ var fs = require('fs');
 var write = require('./writeFile.js');
 var reorderID = require('./rearrangeByID.js');
 var reorderEmail = require('./rearrangeByEmail.js');
+var chooseValue = require('./chooseValue.js');
 
 module.exports = {
   //reads file passed to it, returns array of objects to filter
   readFile : function (file) {
     fs.readFile(file, 'utf8', function(err, data) {
         var file_data = JSON.parse(data).leads;
-        // console.log(file_data);
-        // console.log(reorderID.reOrderID(file_data));
-        // console.log(reorderEmail.reOrderEmail(file_data));
-        write.writeFile('farts.json', reorderID.reOrderID(file_data));
+        var idOrder = reorderID.reOrderID(file_data);
+        console.log(idOrder);
+        //write.writeFile('farts.json', reorderID.reOrderID(file_data));
       });
   }
 }
